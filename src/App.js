771a64e2425d16dc7ddc08930c2a521c3281508c
Eldorado.JM <119120263+eldorado-22@users.noreply.github.com>
2023-07-16 13:45:10 +0600

    hero-block (#1)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import Header from "./components/common/heading/header";
-import {BrowserRouter as Router, Switch} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import React from "react";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
 
 function App() {
     return (
@@ -9,9 +11,8 @@ function App() {
             <Router>
                 <Header/>
                 <Switch>
-                    {/*<Route path="/about">*/}
-                    {/*    <About />*/}
-                    {/*</Route>*/}
+                    <Route path="/" exact component={Home}/>
+                    <Route path="/about" exact component={About}/>
                 </Switch>
             </Router>
         </>
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Hero from "./hero/Hero";
+
+const Home = () => {
+    return (
+        <>
+            <Hero/>
+            {/*<h1>home</h1>*/}
+        </>
+    );
+};
+
+export default Home;
